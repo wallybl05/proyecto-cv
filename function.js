22a0ca2e4973efbdd88b60b679234c12657fba9c
wallybl05 <123819246+wallybl05@users.noreply.github.com>
2023-01-28 19:02:02 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+document.getElementById("cambiar color").onclick=function(){
+    document.body.style.backgroundColor = "#487160"
+}
+document.getElementById("color original").onclick=function(){
+    document.body.style.backgroundColor = "#DBDBB8"
+}
